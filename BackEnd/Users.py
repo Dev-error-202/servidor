1dca998a5118d54749a143c5a0b13850a564b47a
Aecio <aecinhosilva8@gmail.com>
2026-03-23 23:41:31 -0300

    Modificações no gerenciador de usuarios e inicio da criação do motor do banco

--- BackEnd/Users.py
+++ BackEnd/Users.py
@@ -1,44 +1,60 @@
 import json
-from os import makedirs
-from os.path import dirname
+import os
+import secrets
 
 class Users:
     def __init__(self, local):
-        makedirs(dirname(local), exist_ok=True)
-        try:
-            self.arquivo = open(local, 'r+', encoding='UTF-8')
-        except FileNotFoundError:
-            self.arquivo = open(local, 'w+', encoding='UTF-8')
+        self.local = local
+        os.makedirs(os.path.dirname(local), exist_ok=True)
 
-        self.date: dict = json.load(self.arquivo)
+        if not os.path.exists(local):
+            with open(local, 'w', encoding='utf-8') as f:
+                json.dump({}, f)
 
+        with open(local, 'r', encoding='utf-8') as f:
+            try:
+                self.data = json.load(f)
+            except json.JSONDecodeError:
+                self.data = {}
 
     def getusers(self) -> dict:
-        return self.date
+        return self.data
 
     def commit(self):
-        """
-        Salva os valores no arquivo
-        :return:
-        """
-        self.arquivo.seek(0)
-        self.arquivo.write(json.dumps(self.date, indent=4, ensure_ascii=False))
-        self.arquivo.truncate()
+        temp_file = self.local + ".tmp"
 
-    def cadrastro(self, usuario, **kwargs):
-        if usuario in self.date.keys():
-            raise ValueError('Usuário ja existente')
+        with open(temp_file, 'w', encoding='utf-8') as f:
+            json.dump(self.data, f, indent=4, ensure_ascii=False)
 
-        self.date.update({usuario: kwargs})
+        os.replace(temp_file, self.local)  # write atômico
+
+    def cadastro(self, usuario, **kwargs):
+
+        if usuario in self.data:
+            raise ValueError('Usuário já existente')
+
+        cod = secrets.token_hex(16)
+        user_id = '-'.join(cod[i:i+8] for i in range(0, len(cod), 8)).upper()
+
+        kwargs['id'] = user_id
+        self.data[usuario] = kwargs
+
+        return user_id  # útil pra criar pasta
 
     def update(self, usuario, **kwargs):
-        '''
-        Altera um ou mais valores dentro do tabela de usuarios
-        :param usuario: Usuario existente no banco de dador
-        :param kwargs: Parametros de atualização ou inclusão
-        :return:
-        '''
-        self.date[usuario].update(kwargs)
+        if usuario not in self.data:
+            raise ValueError('Usuário não existe')
+
+        self.data[usuario].update(kwargs)
+
+    def delUser(self, usuario):
+        del self.data[usuario]
 
 
+
+
+f = Users(r'C:\Users\aecin\Documents\PythonProjects\Testes\users.json')
+
+f.delUser('aecio')
+f.commit()
 
--- BackEnd/banco_core.py
+++ BackEnd/banco_core.py
@@ -4,3 +4,25 @@
 """
 
 import os
+from random import randint
+import sys
+import sqlite3 as sql
+import platform
+
+if platform.system() == 'Windows':
+    pastamain = 'C:/ProgramData/BancoSystem'
+else:
+    pastamain = '/var/BancoSystem'
+
+
+
+
+
+
+class BancoCore:
+    def __init__(self, user):
+        self.pathuser = os.path.join(pastamain, user)
+
+
+
+
